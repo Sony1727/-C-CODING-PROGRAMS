413fb1cd8db8fa3e6842382e14c98d938215bb20
SONY DADU <78530922+Sony1727@users.noreply.github.com>
2023-11-18 09:48:35 +0530

    Create NEGATIVE OR POSITIVE.C

---
+++ NEGATIVE OR POSITIVE.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num;
+printf("enter the number");
+scanf("%d",&num);
+if(num<0){
+printf("NUMBER IS NEGATIVE");
+}
+else
+{
+printf("NUMBER IS POSITIVE");
+}
+}
