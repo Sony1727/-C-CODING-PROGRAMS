05894f06e1064376b74871bf8dd8ef77e290cf5a
SONY DADU <78530922+Sony1727@users.noreply.github.com>
2023-11-20 08:59:07 +0530

    Create HEIGHT GREATER OR LESSER.C

---
+++ HEIGHT GREATER OR LESSER.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int h;
+    printf("Enter the height");
+    scanf("%d",&h);
+if(h<0)
+{
+    printf("HEIGHT IS SMALLER");
+}
+else
+{
+    printf("HEIGHT IS GREATER");
+}
+    return 0;
+}
