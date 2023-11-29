a206b3e25a656878c05e6f667d7aee61076df56c
SONY DADU <78530922+Sony1727@users.noreply.github.com>
2023-11-29 11:39:07 +0530

    Create MULTIDIMENSIONAL ARRAY.C

---
+++ MULTIDIMENSIONAL ARRAY.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,j,values[3][3];
+    printf("enter the values :");
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            scanf("%d",&values[i][j]);
+        }
+    }
+
+printf("entered values are :\n");
+for(i=0;i<3;i++)
+{
+    for(j=0;j<3;j++)
+    {
+        printf("%d \t",values[i][j]);
+    }
+    printf("\n");
+}
+    return 0;
+}
+  
+  
